cc49a6ef7328d9c77dc6e9e801012261331a752c
MartinMarkov-QA <martin.markov1980@gmail.com>
2026-09-05 21:49:51 +0100

    Refactor tests to use Playwright's request API and remove outdated test cases

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { log } from 'node:console';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.use({ ignoreHTTPSErrors: true });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('Get Tags', async ({ request }) => {
+  const responseTags = await request.get('https://conduit-api.bondaracademy.com/api/tags');
+  const tagsJSON = await responseTags.json();
+
+  expect(responseTags.status()).toBe(200);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
