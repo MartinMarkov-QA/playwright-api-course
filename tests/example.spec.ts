39c3d61c5ec465d7ca12971c022b7058cebf000c
MartinMarkov-QA <martin.markov1980@gmail.com>
2026-09-05 22:01:02 +0100

    Refactor Tags API test structure and improve code formatting

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,13 +1,15 @@
-import { test, expect } from '@playwright/test';
-import { log } from 'node:console';
+import { test, expect } from "@playwright/test";
 
 test.use({ ignoreHTTPSErrors: true });
 
-test('Get Tags', async ({ request }) => {
-  const responseTags = await request.get('https://conduit-api.bondaracademy.com/api/tags');
-  const tagsJSON = await responseTags.json();
+test.describe("Tags API Tests", () => {
+  test("Get Tags", async ({ request }) => {
+    const responseTags = await request.get("https://conduit-api.bondaracademy.com/api/tags");
+    const tagsJSON = await responseTags.json();
 
-  expect(responseTags.status()).toBe(200);
+    expect(responseTags.status()).toBe(200);
+    expect(tagsJSON).toHaveProperty("tags");
+    expect(tagsJSON.tags.length).toBeLessThanOrEqual(10);
+    expect(tagsJSON.tags[0]).toEqual("Test");
+  });
 });
-
-
